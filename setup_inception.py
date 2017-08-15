32c08d451ee7954df97cfb74dadfddb84c763afb
Huan Zhang <ecezhang@ucdavis.edu>
2017-08-15 10:46:48 -0400

    fix inception model download link

--- setup_inception.py
+++ setup_inception.py
@@ -70,7 +70,7 @@ tf.app.flags.DEFINE_integer('num_top_predictions', 5,
                             """Display this many predictions.""")
 
 # pylint: disable=line-too-long
-DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
+DATA_URL = 'http://jaina.cs.ucdavis.edu/datasets/adv/imagenet/inception_v3_2016_08_28_frozen.tar.gz'
 # pylint: enable=line-too-long
 
 
@@ -78,17 +78,13 @@ class NodeLookup(object):
   """Converts integer node ID's to human readable labels."""
 
   def __init__(self,
-               label_lookup_path=None,
-               uid_lookup_path=None):
+               label_lookup_path=None):
     if not label_lookup_path:
       label_lookup_path = os.path.join(
-          FLAGS.model_dir, 'imagenet_2012_challenge_label_map_proto.pbtxt')
-    if not uid_lookup_path:
-      uid_lookup_path = os.path.join(
-          FLAGS.model_dir, 'imagenet_synset_to_human_label_map.txt')
-    self.node_lookup = self.load(label_lookup_path, uid_lookup_path)
+          FLAGS.model_dir, 'labels.txt')
+    self.node_lookup = self.load(label_lookup_path)
 
-  def load(self, label_lookup_path, uid_lookup_path):
+  def load(self, label_lookup_path):
     """Loads a human readable English name for each softmax node.
 
     Args:
@@ -98,38 +94,18 @@ class NodeLookup(object):
     Returns:
       dict from integer node ID to human-readable string.
     """
-    if not tf.gfile.Exists(uid_lookup_path):
-      tf.logging.fatal('File does not exist %s', uid_lookup_path)
     if not tf.gfile.Exists(label_lookup_path):
       tf.logging.fatal('File does not exist %s', label_lookup_path)
 
-    # Loads mapping from string UID to human-readable string
-    proto_as_ascii_lines = tf.gfile.GFile(uid_lookup_path).readlines()
-    uid_to_human = {}
-    p = re.compile(r'[n\d]*[ \S,]*')
-    for line in proto_as_ascii_lines:
-      parsed_items = p.findall(line)
-      uid = parsed_items[0]
-      human_string = parsed_items[2]
-      uid_to_human[uid] = human_string
-
     # Loads mapping from string UID to integer node ID.
-    node_id_to_uid = {}
+    node_id_to_name = {}
     proto_as_ascii = tf.gfile.GFile(label_lookup_path).readlines()
     for line in proto_as_ascii:
-      if line.startswith('  target_class:'):
-        target_class = int(line.split(': ')[1])
-      if line.startswith('  target_class_string:'):
-        target_class_string = line.split(': ')[1]
-        node_id_to_uid[target_class] = target_class_string[1:-2]
-
-    # Loads the final mapping of integer node ID to human-readable string
-    node_id_to_name = {}
-    for key, val in node_id_to_uid.items():
-      if val not in uid_to_human:
-        tf.logging.fatal('Failed to locate: %s', val)
-      name = uid_to_human[val]
-      node_id_to_name[key] = name
+      if line:
+        words = line.split(':')
+        target_class = int(words[0])
+        name = words[1]
+        node_id_to_name[target_class] = name
 
     return node_id_to_name
 
@@ -294,9 +270,6 @@ def main(_):
   maybe_download_and_extract()
   image = (FLAGS.image_file if FLAGS.image_file else
            os.path.join(FLAGS.model_dir, 'cropped_panda.jpg'))
-  # image = "/home/huan/projects/adversarial/nn_robust_attacks/original_0.png"
-  image = "/home/huan/projects/adversarial/imagenetdata/imgs/598.00032612.jpg"
-  image = "/home/huan/projects/adversarial/imagenetdata/imgs/49.00000541.jpg"
   # run_inference_on_image(image)
   create_graph()
   with tf.Session() as sess:
